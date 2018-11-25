ada712b78642ebb2337a0ee5f80f3aaf43959bc6
Laura Darlak <laura@polyporedesign.com>
2018-11-25 17:10:01 +0000

    automatically backed up by learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,6 +1,6 @@
 class CreateStudents < ActiveRecord::Migrate
   def change
-    create_table students: do |t|
+    create_table :students do |t|
       t.string :first_name
       t.string :last_name
     end
